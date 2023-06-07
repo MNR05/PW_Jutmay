b5e3662cebf7231f5d2bd93ef76615d978c05c10
MNR05 <mnr052001@gmail.com>
2023-06-07 03:57:24 -0500

    Orden de las imagenes para el portafolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*TELCOM */
 var swiper = new Swiper(".slide-container1", {
   slidesPerView: 1,
   spaceBetween: 100,
@@ -31,7 +32,7 @@ var swiper = new Swiper(".slide-container1", {
     },
   },*/
 });
-/* */
+/*S_E*/
 var swiper = new Swiper(".slide-container2", {
   slidesPerView: 1,
   spaceBetween: 100,
@@ -66,3 +67,105 @@ var swiper = new Swiper(".slide-container2", {
   },*/
 });
 
+/*F_E */
+var swiper = new Swiper(".slide-container3", {
+  slidesPerView: 1,
+  spaceBetween: 100,
+  sliderPerGroup: 1,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+
+  /*breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },*/
+});
+/*CLI_REF */
+var swiper = new Swiper(".slide-container4", {
+  slidesPerView: 1,
+  spaceBetween: 100,
+  sliderPerGroup: 1,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next3",
+    prevEl: ".swiper-button-prev3",
+  },
+
+  /*breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },*/
+});
+/*CLI_REF */
+var swiper = new Swiper(".slide-container5", {
+  slidesPerView: 1,
+  spaceBetween: 100,
+  sliderPerGroup: 1,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next4",
+    prevEl: ".swiper-button-prev4",
+  },
+
+  /*breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },*/
+});
